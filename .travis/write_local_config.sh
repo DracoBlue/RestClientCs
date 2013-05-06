471e4505d2e550de79a224505c7c03cb8b1ecb62
DracoBlue <JanS@DracoBlue.de>
2013-05-06 23:49:49 +0200

    Fixed creation of debug folder

--- .travis/write_local_config.sh
+++ .travis/write_local_config.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 cd ../
-mkdir RestClientCs/bin/Debug/
+mkdir -p RestClientCs/bin/Debug/
 
 echo '<?xml version="1.0" encoding="utf-8"?>' > RestClientCs/bin/Debug/RestClientCs.dll.config
 echo '<configuration><appSettings><add key="TestServerUrl" value="http://localhost:8080'`pwd`'/.travis/echo.php" /></appSettings></configuration>' > RestClientCs/bin/Debug/RestClientCs.dll.config
